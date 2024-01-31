ff1e57c4163669b1160ffa9e1867b6bf7478c3ef
chenggang.xu <chenggang.xu@xipu.com>
2024-01-31 18:12:11 +0800

    first commit

---
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("haha").password("123456").roles("USER").build());
+        return manager;
+    }
+}
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+@Data
+public class User {
+    @TableId(value = "id" ,type = IdType.AUTO)
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Integer enabled;
+}
