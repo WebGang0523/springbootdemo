83481de7c27e195e028d51458dae700a624ffdf1
chenggang.xu <chenggang.xu@xipu.com>
2024-02-01 10:14:29 +0800

    '..'

---
+++ src/main/java/com/example/demo/config/DBUserDetailManager.java
@@ -0,0 +1,71 @@
+package com.example.demo.config;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import jakarta.annotation.Resource;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class DBUserDetailManager implements UserDetailsManager, UserDetailsPasswordService  {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public UserDetails updatePassword(UserDetails user, String newPassword) {
+        return null;
+    }
+
+    @Override
+    public void createUser(UserDetails user) {
+
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+
+    }
+
+    @Override
+    public void deleteUser(String username) {
+
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+
+    }
+
+    @Override
+    public boolean userExists(String username) {
+        return false;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username",username);
+        User user = userMapper.selectOne(queryWrapper);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        } else {
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            return new org.springframework.security.core.userdetails.User(
+                    user.getUsername(),
+                    user.getPassword(),
+                    user.getEnabled() == 1,
+                    true,
+                    true,
+                    true,
+                    authorities
+            );
+        }
+    }
+}
--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -2,19 +2,43 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withDefaultPasswordEncoder().username("haha").password("123456").roles("USER").build());
+//        return manager;
+//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //authorizeRequests()：开启授权保护
+        //anyRequest()：对所有请求开启授权保护
+        //authenticated()：已认证请求会自动被授权
+        http
+                .authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+//                .formLogin(withDefaults())//表单授权方式
+                .httpBasic(withDefaults());//基本授权方式
+
+        return http.build();
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder().username("haha").password("123456").roles("USER").build());
+        DBUserDetailManager manager = new DBUserDetailManager();
+
         return manager;
     }
 }
